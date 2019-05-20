7dc297e852e7802c0e17a4b2d273bcf4710fbff3
Henrik Olsson <henolsno@deltaww.com>
2019-05-20 09:27:47 +0200

    End of section 12

--- DatingApp.API/Controllers/UsersController.cs
+++ DatingApp.API/Controllers/UsersController.cs
@@ -33,7 +33,7 @@ namespace DatingApp.API.Controllers
             return Ok(usersToReturn);
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name = "GetUser")]
         public async Task<IActionResult> GetUsers(int Id)
         {
             var user = await _repo.GetUser(Id);
